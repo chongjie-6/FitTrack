85b5fc1eaf4f23c7a9dd529f914c88528a2c31be
Chongjie <chongjiechen@outlook.com>
2025-04-19 00:05:00 +1000

    try make more performance changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -105,9 +105,8 @@ export default function Dashboard() {
     }
   };
   useEffect(() => {
-    fetchWeightsLifted()
-    fetchWorkouts()
-  },[]);
+    Promise.all([fetchWeightsLifted(), fetchWorkouts()]);
+  }, []);
 
   const createWorkout = async () => {
     try {
