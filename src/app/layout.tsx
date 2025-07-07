c18a346ec8870a356dbd6ef23f9cf0e87f3d9ddc
Chongjie <chongjiechen@outlook.com>
2025-07-07 16:57:19 +1000

    - add toast to show user feedback when user duplicates a workout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Suspense } from "react";
 import { SkeletonPage } from "@/components/ui/skeleton_page";
+import { Toaster } from "sonner";
 export const metadata: Metadata = {
   title: "Fitness Tracker",
   description: "Track your workouts today!",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="dark">
+        <Toaster></Toaster>
         <Suspense fallback={<SkeletonPage />}>{children}</Suspense>
       </body>
     </html>
--- src/components/ui/duplicateWorkoutButton.tsx
+++ src/components/ui/duplicateWorkoutButton.tsx
@@ -2,7 +2,8 @@
 
 import { DuplicateWorkoutAction } from "@/app/actions/sessions/duplicateWorkout";
 import { useRouter } from "next/navigation";
-
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function DuplicateWorkoutButton({
   sessionId,
@@ -10,17 +11,26 @@ export default function DuplicateWorkoutButton({
   sessionId: string;
 }) {
   const router = useRouter();
+  const [duping, setDuping] = useState(false);
   const onDuplicateClick = async () => {
+    setDuping(true);
     const path = await DuplicateWorkoutAction(sessionId);
-    router.push(path);
+    setDuping(false);
+    if (path) {
+      toast("Workout has been duplicated successfully!");
+      router.push(path);
+    } else {
+      toast(path);
+    }
   };
 
   return (
     <button
+      disabled={duping}
       onClick={onDuplicateClick}
       className="text-blue-500 hover:text-blue-400 font-bold hidden sm:block w-fit cursor-pointer"
     >
-      Duplicate Workout
+      {`${!duping ? `Duplicate Workout` : `Duplicating...`}`}
     </button>
   );
 }
